ca682e15de9e947abbc2a0da62922e34640cc418
Yassin HAMMOU OUALI <y.hammououali@gmail.com>
2022-10-26 09:51:18 +0200

    adding controller

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php
 
-    echo 'Hello World!';
+    // define('HOMEPAGE_PATH', '/');
+    // define('CONTACT_PATH', '/contact');
+
+    // const HOMEPAGE_PATH = '';
+    const CONTACT_PATH = 'contact';
+
+    require_once('views/base.php');
+
+    $requestUri = explode('/', $_SERVER['REQUEST_URI']);
+
+    switch($requestUri[array_key_last($requestUri)]) {
+        /**case HOMEPAGE_PATH:
+            echo 'ACCUEIL';
+            break;*/
+        case CONTACT_PATH:
+            $controller = 'contact';
+            $action = 'contact';
+            echo 'CONTACT';
+            break;
+        default:
+            $controller = 'homepage';
+            $action = 'home';
+            echo 'ACCUEIL';
+    }
+
+    require_once('controllers/'.$controller.'Controller.php');
+
+    /**
+    if($_SERVER['REQUEST_URI'] === '/') {
+        echo 'ACCUEIL';
+    } elseif($_SERVER['REQUEST_URI'] === '/contact') {
+        echo 'CONTACT';
+    }
+    **/
