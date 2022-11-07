334ff060879b9d3253def20c73e599a7fdf68ed5
Yassin HAMMOU OUALI <y.hammououali@gmail.com>
2022-11-07 12:19:42 +0100

    adding database connection

--- controllers/figureController.php
+++ controllers/figureController.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once('models/figure.php');
+require_once('lib/database.php');
+require_once('repositories/figureRepository.php');
+
 final class FigureController
 {
     function create(): void
@@ -8,6 +12,17 @@ final class FigureController
 
         if ('POST' === $_SERVER['REQUEST_METHOD']) {
             $isSent = true;
+
+            $figure = new Figure();
+            $figure->setName($_POST['name']);
+            $figure->setDescription($_POST['description']);
+            $figure->setPicturePath($_POST['picture']);
+            $figure->setVideoPath($_POST['video']);
+
+            $figureRepository = new FigureRepository();
+            $figureRepository->setConnection(new DatabaseConnection());
+
+            $figureRepository->create($figure);
         }
 
         require_once('views/pages/figure/create.php');
